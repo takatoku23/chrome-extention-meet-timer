43331b84e2baef2962a08025e44f914855a0a8e2
takatoku23 <takutoplayer1211@icloud.com>
2022-09-24 23:57:19 +0900

    環境構築〜基本タイマー機能

---
+++ src/openChat.ts
@@ -0,0 +1,13 @@
+export function openChat() {
+  // chatを開く
+  const googleMaterialIcon = document.getElementsByClassName("google-material-icons") as HTMLCollectionOf<HTMLElement>;
+  let chatIconElement!: HTMLElement;
+  for (let i = 0; i < googleMaterialIcon.length; i++) {
+    if (googleMaterialIcon[i].textContent === "chat") {
+      chatIconElement = googleMaterialIcon[i];
+      break;
+    }
+  }
+  if (chatIconElement !== undefined)
+    chatIconElement.click();
+}
