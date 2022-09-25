eed48fe9dc68c6ad84c2079631d0c60cf2fa49d1
takatoku23 <takutoplayer1211@icloud.com>
2022-09-25 17:19:25 +0900

    多分必要なかったbackground.jsを初期化

--- src/background.ts
+++ src/background.ts
@@ -1,14 +1,1 @@
-type Url = {
-  url: string
-}
-chrome.runtime.onMessage.addListener(() => {
-  
-});
-function notify(message: Url) {
-  chrome.notifications.create({
-    "type": "basic",
-    "iconUrl": chrome.extension.getURL("link.png"),
-    "title": "リンクをクリックしました!",
-    "message": message.url
-  });
-}
+// ここにタイマーの機能を入れるべき？
