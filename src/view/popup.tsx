dd93edc50ebf614d12fd71c6170304273a0d20b7
takatoku23 <takutoplayer1211@icloud.com>
2023-01-04 19:06:49 +0900

    prettier

--- src/view/popup.tsx
+++ src/view/popup.tsx
@@ -7,17 +7,14 @@ export const Popup = () => {
   const [clearTimer, setClearTimer] = useState(false);
 
   useEffect(() => {
-    if(startedTimer) {
+    if (startedTimer) {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tab = tabs[0];
         if (tab.id) {
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              message: "startTimer",
-              value: totalMinutes
-            }
-          );
+          chrome.tabs.sendMessage(tab.id, {
+            message: "startTimer",
+            value: totalMinutes,
+          });
         }
       });
     }
@@ -25,16 +22,13 @@ export const Popup = () => {
   }, [startedTimer]);
 
   useEffect(() => {
-    if(clearTimer) {
+    if (clearTimer) {
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         const tab = tabs[0];
         if (tab.id) {
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              message: "clearTimer"
-            }
-          );
+          chrome.tabs.sendMessage(tab.id, {
+            message: "clearTimer",
+          });
         }
       });
     }
@@ -45,25 +39,27 @@ export const Popup = () => {
    * タイマーハンドル
    */
   const handleStartTimer = () => {
-    if(!totalMinutes || totalMinutes < 0 || !Number.isInteger(totalMinutes)) {
+    if (!totalMinutes || totalMinutes < 0 || !Number.isInteger(totalMinutes)) {
       alert("正しい値を入力してください");
       return;
     }
     setStartedTimer(true);
-  }
+  };
 
   const handleClearTimer = () => {
     setClearTimer(true);
-  }
+  };
 
   const handleTotalMinutesInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTotalMinutes(Number(e.target.value));
-  }
+  };
   return (
     <>
       <h1 className="text-lg">時間を入力してください</h1>
       <div className=" space-y-4">
-        <p className=" text-xs">＊入力した時間が半分、全て経過した時のみmeetのチャットを送信します。</p>
+        <p className=" text-xs">
+          ＊入力した時間が半分、全て経過した時のみmeetのチャットを送信します。
+        </p>
         <div>
           <label
             htmlFor="minutes"
@@ -76,7 +72,7 @@ export const Popup = () => {
             id="minutes"
             max={60}
             min={1}
-            onChange={handleTotalMinutesInput} 
+            onChange={handleTotalMinutesInput}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1"
             placeholder=""
             required
@@ -86,13 +82,13 @@ export const Popup = () => {
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block w-full"
             onClick={() => handleStartTimer()}
-            >
+          >
             start
           </button>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block w-full"
             onClick={() => handleClearTimer()}
-            >
+          >
             clear
           </button>
         </div>
